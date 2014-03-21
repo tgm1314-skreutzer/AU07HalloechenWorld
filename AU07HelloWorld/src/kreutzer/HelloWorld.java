9ba65147b7ff143b767ba70d25b1812239ed39d1
tgm1314-skreutzer <skreutzer@student.tgm.ac.at>
2014-03-21 10:45:40 +0100

    Zweite Methode

--- AU07HelloWorld/src/kreutzer/HelloWorld.java
+++ AU07HelloWorld/src/kreutzer/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello " + name);
 	}
 	public static void holaWorld (String name){
-		System.out.println("Hola " + name);
+		System.out.println("Holaa " + name);
 	}
 }
